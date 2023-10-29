898af6d7e99576511396bd3d9e61f058b83f7877
gabriel.lion <gabriel.lion@epita.fr>
2023-10-29 20:20:23 +0100

    Update cors

--- backend/main.go
+++ backend/main.go
@@ -21,6 +21,22 @@ const (
 	psql_dbname   = "crm"
 )
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	// Initialize the Gin router
 	router := gin.Default()
@@ -29,7 +45,7 @@ func main() {
 	// config.AllowOrigins = []string{"*"} 
 	// config.AllowAllOrigins = true
 	// router.Use(cors.New(config))
-	router.Use(cors.Default())
+	router.Use(CORS())
 
 	// Initialize the database connection
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
