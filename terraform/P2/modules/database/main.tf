5205278253f5a70ace95fa1d0b86734a6142ecdd
gabriel.lion <gabriel.lion@epita.fr>
2023-11-02 00:46:46 +0100

    [FIX] update security group for database

--- terraform/P2/modules/database/main.tf
+++ terraform/P2/modules/database/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "database" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = var.webapp_sg_id
     self = true
   }
 
@@ -27,6 +27,7 @@ resource "aws_security_group" "database" {
 resource "aws_instance" "database" {
   ami           = var.ami
   instance_type = var.instance_type
+  security_groups = [aws_security_group.database.id]
   key_name      = var.ssh_key_name
   subnet_id     = var.private_subnet_ids[0]
   user_data = var.user_data
