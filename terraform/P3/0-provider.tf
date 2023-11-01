fcb6d17dff4c544daf8124e18aa66ec34f2c7dea
Jocelyn Q <jocelyn.develop@gmail.com>
2023-11-01 19:12:05 +0100

    [INIT] Terraform files

---
+++ terraform/P3/0-provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
