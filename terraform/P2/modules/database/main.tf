ee4ad2845c5bba61af611cc7cf1a13427b653fd5
gabriel.lion <gabriel.lion@epita.fr>
2023-11-02 12:57:43 +0100

    [FIX] DB security group

--- terraform/P2/modules/database/main.tf
+++ terraform/P2/modules/database/main.tf
@@ -11,12 +11,18 @@ resource "aws_security_group" "database" {
     self = true
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    security_groups = var.webapp_sg_id
-    self = true
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
