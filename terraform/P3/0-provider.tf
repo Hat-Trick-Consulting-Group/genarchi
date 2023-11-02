c41c83abddebbf313c20c2d5f760772412ffb0ee
Charizared <jocelyn.develop@gmail.com>
2023-11-02 12:10:44 +0100

    [P3][TERRAFORM] Ajout base TC pour setup les lambda Terraform (#8)
    * [INIT] Terraform files
    
    * [ADD] deployed health func

---
+++ terraform/P3/0-provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.3.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
