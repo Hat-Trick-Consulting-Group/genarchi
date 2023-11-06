341fc2ab8fb46a8d61a757ab9144d5171468b141
Jocelyn Q <jocelyn.develop@gmail.com>
2023-11-06 14:36:41 +0100

    [ADD] docker-compose for go + dockerfile + network

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-
-	"main/psql"
-	"main/routes"
 )
 
 // PostgreSQL represents the database connection information
@@ -48,16 +42,6 @@ func main() {
 
 	log.Println("Environment: " + env)
 
-	psql_host     := os.Getenv("PSQL_HOST")
-	psql_port, err    := strconv.Atoi(os.Getenv("PSQL_PORT"))
-	psql_user     := os.Getenv("PSQL_USER")
-	psql_password := os.Getenv("PSQL_PASSWORD")
-	psql_dbname   := os.Getenv("PSQL_DBNAME")
-	
-	if err != nil {
-		log.Fatal("Error converting PSQL_PORT to int")
-	}
-
 	// Initialize the Gin router
 	router := gin.Default()
 
@@ -67,32 +51,26 @@ func main() {
 	// router.Use(cors.New(config))
 	router.Use(CORS())
 
-	// Initialize the database connection
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable connect_timeout=0",
-		psql_host, psql_port, psql_user, psql_password, psql_dbname)
-	fmt.Printf("psqlInfo: %s\n", psqlInfo)
 
-	db := psql.InitDB(psqlInfo)
+	// // Create the "clients" table if it doesn't exist
+	// if err := psql.CreateClientsTable(db); err != nil {
+	// 	log.Fatalf("Failed to create 'clients' table: %v", err)
+	// }
 
-	// Create the "clients" table if it doesn't exist
-	if err := psql.CreateClientsTable(db); err != nil {
-		log.Fatalf("Failed to create 'clients' table: %v", err)
-	}
-
-	// Add routes to handle API requests
-	router.GET("/health", routes.GetStatusHandler)
-	router.POST("/add-client", func(c *gin.Context) {
-        routes.CreateClientHandler(c, db)
-    })
-	router.GET("/get-clients", func(c *gin.Context) {
-		routes.GetClientsHandler(c, db)
-	})
-	router.PUT("/update-client", func(c *gin.Context) {
-		routes.UpdateClientHandler(c, db)
-	})
-	router.DELETE("/delete-client", func(c *gin.Context) {
-		routes.DeleteClientHandler(c, db)
-	})
+	// // Add routes to handle API requests
+	// router.GET("/health", routes.GetStatusHandler)
+	// router.POST("/add-client", func(c *gin.Context) {
+    //     routes.CreateClientHandler(c, db)
+    // })
+	// router.GET("/get-clients", func(c *gin.Context) {
+	// 	routes.GetClientsHandler(c, db)
+	// })
+	// router.PUT("/update-client", func(c *gin.Context) {
+	// 	routes.UpdateClientHandler(c, db)
+	// })
+	// router.DELETE("/delete-client", func(c *gin.Context) {
+	// 	routes.DeleteClientHandler(c, db)
+	// })
 
 
 	// Start the API server
