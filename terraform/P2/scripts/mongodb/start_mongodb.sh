ae39728630693cdc2fff17b83689c81cb95acf92
gabriel.lion <gabriel.lion@epita.fr>
2023-11-07 19:45:20 +0100

    [ADD] Documentation

--- terraform/P2/scripts/mongodb/start_mongodb.sh
+++ terraform/P2/scripts/mongodb/start_mongodb.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-sudo systemctl status mongod
 sudo cp -rf ./mongod.conf /etc/
 sudo systemctl start mongod
+sudo systemctl status mongod
 
-# load configuration
+# load configuration #TODO: update for later when implementing replication
 # sudo mongosh --file setup_db.js
-
-# mongodb commands
-
-# sudo systemctl start mongod
-# sudo systemctl status mongod
-# sudo systemctl enable mongod
-# sudo systemctl stop mongod
-# sudo systemctl restart mongod
