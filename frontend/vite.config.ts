b03a9b5ead20ac01b092916aa0d75537f0280e14
Jocelyn Q <jocelyn.develop@gmail.com>
2023-11-14 12:59:34 +0100

    [FIX] Changed output build dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+  plugins: [preact()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "dist/",
+        entryFileNames: "app-[name].js",
+        assetFileNames: "app-[name].css",
+        chunkFileNames: "chunk-[name].js",
+        manualChunks: undefined,
+      },
+    },
+  },
 });
