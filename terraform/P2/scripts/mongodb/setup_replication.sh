51111d79407cf8763a1dcabd31561d7c3b97c34a
gabriel.lion <gabriel.lion@epita.fr>
2023-11-26 13:39:48 +0100

    [FIX] clean comments

--- terraform/P2/scripts/mongodb/setup_replication.sh
+++ terraform/P2/scripts/mongodb/setup_replication.sh
@@ -6,11 +6,10 @@
 MONGO_HOST_1=${machine_ip_1}
 MONGO_HOST_2=${machine_ip_2}
 MONGO_HOST_3=${machine_ip_3}
-REPLICA_SET_NAME="rs0"
 
 # MongoDB shell command to initiate the replica set
 mongosh --eval "rs.initiate({
-  _id: '$REPLICA_SET_NAME',
+  _id: 'rs0',
   members: [
     { _id: 0, host: '$MONGO_HOST_1:27017' },
     { _id: 1, host: '$MONGO_HOST_2:27017' },
