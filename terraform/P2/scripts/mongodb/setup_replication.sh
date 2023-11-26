f5047f467ec6f4365b00ca6b700b4865a85bef52
gabriel.lion <gabriel.lion@epita.fr>
2023-11-26 19:55:39 +0100

    [FIX] add more debbuging and a wait for mongod to start

--- terraform/P2/scripts/mongodb/setup_replication.sh
+++ terraform/P2/scripts/mongodb/setup_replication.sh
@@ -2,10 +2,24 @@
 
 # File to be called in the primary node only
 
+# Wait for mongod to start
+sleep 10
+
 # Get the machine's IP address dynamically
 MONGO_HOST_1=${db_host_ip_1}
 MONGO_HOST_2=${db_host_ip_2}
 MONGO_HOST_3=$(hostname -I | awk '{print $1}')
+
+# Print the MongoDB shell command for debugging
+echo "Executing MongoDB shell command:"
+echo "mongosh --eval \"rs.initiate({
+  _id: 'rs0',
+  members: [
+    { _id: 0, host: '$MONGO_HOST_1:${db_port}' },
+    { _id: 1, host: '$MONGO_HOST_2:${db_port}' },
+    { _id: 2, host: '$MONGO_HOST_3:${db_port}' }
+  ]
+})\""
 
 # MongoDB shell command to initiate the replica set
 mongosh --eval "rs.initiate({
