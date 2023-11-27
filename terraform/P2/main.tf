9da73233a7d9a8b7bdfcf5a1bc7ed0ee9ae87fdb
gabriel.lion <gabriel.lion@epita.fr>
2023-11-27 16:56:44 +0100

    [FIX] Number of public subnets

--- terraform/P2/main.tf
+++ terraform/P2/main.tf
@@ -21,8 +21,8 @@ locals {
 module "vpc" {
   source               = "./modules/vpc"
   vpc_cidr             = "10.0.0.0/16"
-  public_subnets_cidr  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  public_subnets_cidr  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets_cidr = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   azs                  = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
 }
 
