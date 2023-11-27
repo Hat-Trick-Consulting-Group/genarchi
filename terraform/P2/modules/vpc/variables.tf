97e5a31fa565816eca7485e7cdcfb0a8b99f9c71
gabriel.lion <gabriel.lion@epita.fr>
2023-11-27 17:24:22 +0100

    [FIX] number of public and private subnets should be equal

--- terraform/P2/modules/vpc/variables.tf
+++ terraform/P2/modules/vpc/variables.tf
@@ -5,6 +5,11 @@ variable "vpc_cidr" {
 variable "public_subnets_cidr" {
   type    = list(any)
   default = ["10.0.1.0/24"]
+
+  validation {
+    condition     = length(var.public_subnets_cidr) == length(var.private_subnets_cidr)
+    error_message = "The length of list1 must be less than or equal to the length of list2."
+  }
 }
 
 variable "private_subnets_cidr" {
