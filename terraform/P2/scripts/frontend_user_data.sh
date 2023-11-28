dc734d62d51d6fc991778c4fc46fb9f8908c8a72
Emeric Renaud <emeric.renaud7@gmail.com>
2023-11-28 16:02:35 +0100

    DONE installation scripts

--- terraform/P2/scripts/frontend_user_data.sh
+++ terraform/P2/scripts/frontend_user_data.sh
@@ -4,12 +4,6 @@
 sudo yum update -y
 sudo yum install -y docker git
 
-# Install Go (adjust the version as needed)
-sudo yum install -y golang
-export GOPATH=/go
-export GOCACHE=/go/cache
-export PATH=$PATH:$GOPATH/bin
-
 # Install Node.js and npm
 sudo yum install https://rpm.nodesource.com/pub_21.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
@@ -21,17 +15,8 @@ git clone https://github.com/Hat-Trick-Consulting-Group/genarchi.git
 cd genarchi
 git checkout ${git_branch}
 
-# Change directory to the backend and run your Go application
-cd backend
-# Switch to the production environment to use .env.production
-export GO_ENV=production
-# Set .env.production variables
-echo "MONGO_URI=mongodb://${db_host_ip_1}:${db_port},${db_host_ip_2}:${db_port},${db_host_ip_3}:${db_port}/?replicaSet=rs0" >> .env.production
-
-go run main.go &
-
 # Change directory to the frontend and run your React application
-cd ../frontend
+cd frontend/
 echo "VITE_API_URL=http://${alb_dns_name}:8080" >> .env.production
 cat .env.production
 npm install
