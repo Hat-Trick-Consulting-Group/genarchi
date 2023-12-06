d032ce0604cf5f9a3eef4c6d66ee3b10825969f3
Emeric Renaud <emeric.renaud7@gmail.com>
2023-12-06 17:40:52 +0100

    Fix

--- k6_tests/tests1_add-clients.js
+++ k6_tests/tests1_add-clients.js
@@ -12,7 +12,6 @@ export default function () {
     "email": "john@example.com",    
   };
 
-  console.log(payload);
   const params = {
     headers: {
       'Content-Type': 'application/json',
@@ -22,14 +21,8 @@ export default function () {
   //Post request
   let res = http.post(url + '/add-client', JSON.stringify(payload), params);
   
-  console.log(res.status, res.body);
   //Check response
   check(res, {
     'status is 201': (r) => r.status === 201,
   });
-  
-  sleep(1);
 }
-
-//front http://hat-trick-tf-front-end-bucket.s3-website.eu-west-3.amazonaws.com
-//back https://ghc0h59rck.execute-api.eu-west-3.amazonaws.com/prod
