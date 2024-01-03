50c3c7e929b8493351316515e30ff7e1b2071df3
gabrielion <47116775+gabrielion@users.noreply.github.com>
2024-01-03 10:33:42 +0100

    [P3] Install npm on windows (#34)
    * [ADD] NPM and NODE install on windows 10 powershell
    
    * [FIX] Bucket name length < 63 car

--- terraform/P3/5.0-build-frontend.tf
+++ terraform/P3/5.0-build-frontend.tf
@@ -5,15 +5,38 @@ resource "local_file" "api_config" {
   EOT
 }
 
+resource "null_resource" "install_nodejs" {
+  depends_on = [local_file.api_config]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      # Check if Node.js is already installed
+      if (-not (Test-Path "$env:ProgramFiles\nodejs\node.exe")) {
+        # Download and install Node.js
+        winget install --uninstall-previous OpenJS.NodeJS
+      } else {
+          Write-Host "Node.js is already installed."
+      }
+    EOT
+
+    interpreter = ["PowerShell", "-Command"]
+  }
+}
+
 resource "null_resource" "build_frontend" {
+  depends_on = [null_resource.install_nodejs]
   triggers = {
     build_trigger = "../../frontend/.env.production"
   }
 
   provisioner "local-exec" {
-    command = "npm install vite@latest && npm install npm install && npm run build"
+    command = <<-EOT
+      if (-not $env:Path.Contains('C:\\Program Files\\nodejs')) {
+        $env:Path += ';C:\\Program Files\\nodejs'
+      }
+      & 'C:\\Program Files\\nodejs\\npm.cmd' install ; & 'C:\\Program Files\\nodejs\\npm.cmd' install vite@latest ; & 'C:\\Program Files\\nodejs\\npm.cmd' run build
+    EOT
     working_dir = "../../frontend/"
+    interpreter = ["PowerShell", "-Command"]
   }
-
-  depends_on = [local_file.api_config]
 }
